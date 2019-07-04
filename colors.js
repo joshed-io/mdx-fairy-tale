9c89d852510928aa25f088a67d58a9fc10f5897e
dzello <jdzielak@gmail.com>
2019-07-05 00:29:36 +0200

    CLeaner background image handling

--- colors.js
+++
@@ -1,6 +0,0 @@
-const colors = {
-  black: `#161616`,
-  lightOrange: `#F9AC01`
-}
-
-export default colors
--- components/provider.js
+++ components/provider.js
@@ -12,11 +12,24 @@ const Provider = ({ children, theme, index, slides }) => (
       position: 'absolute',
       width: '100vw',
       height: '100vh',
+      zIndex: '0',
+      background: "url('/images/fairy-tale-castle.jpg') no-repeat center center",
+      backgroundSize: "cover",
+    }}></div>
+    <div css={{
+      position: 'absolute',
+      width: '100vw',
+      height: '100vh',
       background: '#FFFFF0',
       opacity: '0.80',
-      zIndex: '0'
+      zIndex: '1',
     }}></div>
+    <div css={{
+      position: 'relative',
+      zIndex: 2
+    }}>
     {children}
+    </div>
     {/* <Footer theme={theme} index={index} /> */}
   </div>
 )
